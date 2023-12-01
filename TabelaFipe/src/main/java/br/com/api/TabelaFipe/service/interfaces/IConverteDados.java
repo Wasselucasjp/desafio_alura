64eb95689ea0fd30321ca5dd009c99b7e08233c0
Wasse Lucas <wasselucas.silva@gmail.com>
2023-12-01 11:26:18 -0300

    interface para obterlista de dados

--- TabelaFipe/src/main/java/br/com/api/TabelaFipe/service/interfaces/IConverteDados.java
+++ TabelaFipe/src/main/java/br/com/api/TabelaFipe/service/interfaces/IConverteDados.java
@@ -1,5 +1,9 @@
 package br.com.api.TabelaFipe.service.interfaces;
 
+import java.util.List;
+
 public interface IConverteDados {
     <T> T obterDados(String json, Class<T> classe);
+    <T>List<T>obterLista(String json, Class<T>classe);
+
 }
